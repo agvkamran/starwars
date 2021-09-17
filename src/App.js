a2aa497f9d12e9e83f4d699ff2afbf76bedff588
AndreyFatiyev <andreyfatiyev@gmail.com>
2021-09-17 13:28:59 +0400

    add preloader component

--- src/App.js
+++ src/App.js
@@ -8,16 +8,57 @@ import People from './components/people/people';
 import About from './components/about/about';
 
 class App extends React.Component {
-  
-  constructor() {
+
+  constructor(props) {
     super();
+    // this.state = {
+    //   people: []
+    // }
   }
+
+  // getLocation = () => {
+
+  // }
+
+  // api = async () => {
+  //   const response = await fetch('https://swapi.dev/api/people/?page=1')
+  //     .then((response) => {
+  //      return response.json()
+  //     })
+  //     .then((data) => {
+  //       return data
+  //     })
+  //   this.setState({
+  //     people: response.results
+  //   })
+  // }
+
+  // api = async () => {
+  //   const response = await fetch(`https://swapi.dev/api/${planets}/?page={4}`)
+  //     .then((response) => {
+  //      return response.json()
+  //     })
+  //     .then((data) => {
+  //       return data
+  //     })
+  //   this.setState({
+  //     people: response.results
+  //   })
+  // }
+
+
+
+
+  // componentDidMount() {
+  //   this.api()
+  // }
 
   render() {
     return (
       <BrowserRouter>
         <div className="app">
           <Header />
+          {/* <Route path='/people' render={() => <People people={this.state.people} />} /> */}
           <Route path='/people' component={People} />
           <Route exact path='/' component={Container} />
           <Route path='/about' component={About} />
--- src/components/people/people.jsx
+++ src/components/people/people.jsx
@@ -1,28 +1,40 @@
 import React, { useEffect, useState } from 'react';
+import Preloader from '../../components/preloader/preloader';
 import './people.css'
 
 const People = () => {
-    const [people, setPeople] = useState({results: []});
-    
-    const swApi = async () => {
-        const response = await fetch('https://swapi.dev/api/people')
-          .then((peopleData) => peopleData.json()).catch(e => console.log('swApi', e))
-        setPeople(response);
-      }
+    const [people, setPeople] = useState({ results: [], urlPage: [] });
+    const [isPreloader, setPreloader] = useState(true);
+
+    const swApi = async (pageNumber) => {
+        const response = await fetch(`https://swapi.dev/api/people/?page=${pageNumber}`)
+            .then((peopleData) => peopleData.json()).catch(e => console.log('swApi', e))
+        console.log(response);
+        setPeople({ ...people, results: [...people.results, ...response.results] });
+    }
+
+    console.log(people);
 
     useEffect(() => {
-        console.log('people useEffect', people)
-        swApi();
+        swApi(1);
     }, []);
+
+    const loadMore = () => {
+        swApi(2)
+    }
 
     return (
         people?.results?.map((item) => {
             return (
-                <div className='item'>{item.name}</div>
+                <div>
+                    <button onClick={loadMore}>LoadMore</button>
+                    <div className='item'>{item.name}</div>
+                </div>
             )
         })
     )
 
 }
+
 
 export default People;
